b599ff86d825a0230fb2c1cf777842a6666e5304
koiusa <71818379+koiusa@users.noreply.github.com>
2023-02-07 01:17:31 +0900

    Update compose.sh

--- jenkins/compose.sh
+++ jenkins/compose.sh
@@ -2,6 +2,7 @@
 
 docker pull jenkins/jenkins
 docker build -t jenkins/jenkins .
+docker network create jenkins
 docker-compose -f docker-compose.yml up -d
 sudo chown -R ${USER}:${USER} jenkins-*
 
