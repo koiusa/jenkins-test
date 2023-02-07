0d6b1cccee163403bce8abc82df0de7adbbe954e
koiusa <71818379+koiusa@users.noreply.github.com>
2023-02-08 04:30:28 +0900

    Update compose.sh

--- jenkins/compose.sh
+++ jenkins/compose.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+# docker compose
 docker pull jenkins/jenkins
 docker build -t jenkins/jenkins .
 docker network create jenkins
 docker-compose -f docker-compose.yml up -d
 sudo chown -R ${USER}:${USER} jenkins-*
 
+# plugins
+## install discord-notifier
+docker exec jenkins-jenkins-blueocean-1 bash jenkins-plugin-cli --plugins discord-notifier:218.ve2b_887e3a_f28
