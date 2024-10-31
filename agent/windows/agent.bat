5c7d7e55b23b6c5954c2cd96387be707067b5ac8
koiusa <71818379+koiusa@users.noreply.github.com>
2024-10-31 02:03:26 +0000

    Submodule the agent

--- agent/windows/agent.bat
+++
@@ -1,7 +0,0 @@
-for /f "tokens=1,* delims==" %%a in (setting.ini) do (
-    set %%a=%%b
-)
-
-curl.exe -sO %url%/jnlpJars/agent.jar
-java -jar agent.jar -url %url% -secret %secret% -name %name% -workDir %workdir%
-
